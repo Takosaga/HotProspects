4ae6e73802e61a781983dcfe35e855d8fd1e78cf
Gonzalo Gamez <takosaga@gmail.com>
2020-05-13 19:07:27 -0500

    imported package for qr scanner. added scanner

--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Gamez. All rights reserved.
 //
 
+import CodeScanner
 import SwiftUI
 
 struct ProspectsView: View {
@@ -14,6 +15,7 @@ struct ProspectsView: View {
     }
     
     @EnvironmentObject var prospects: Prospects
+    @State private var isShowingScanner = false
     let filter: FilterType
     
     var title: String {
@@ -53,14 +55,30 @@ struct ProspectsView: View {
             }
                 .navigationBarTitle(title)
                 .navigationBarItems(trailing: Button(action: {
-                    let prospect = Prospect()
-                    prospect.name = "Gonzalo Gamez"
-                    prospect.emailAddress = "gonzalo.gamez@myhisd.net"
-                    self.prospects.people.append(prospect)
+                    self.isShowingScanner = true
                 }) {
                     Image(systemName: "qrcode.viewfinder")
                     Text("Scan")
                 })
+                .sheet(isPresented: $isShowingScanner) {
+                    CodeScannerView(codeTypes: [.qr], simulatedData: "Gonzalo Gamez\ngonzalo.gamez@myhisd.net.com", completion: self.handleScan)
+            }
+        }
+    }
+    
+    func handleScan(result: Result<String, CodeScannerView.ScanError>) {
+        self.isShowingScanner = false
+        switch result {
+        case .success(let code):
+            let details = code.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+            
+            let person = Prospect()
+            person.name = details[0]
+            person.emailAddress = details[1]
+            self.prospects.people.append(person)
+        case .failure(let error):
+            print("Scanning failed")
         }
     }
 }
